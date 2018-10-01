7bed7418aa411950221c062df694a5b8cf77b79d
carly <carly@noukod.com>
2018-10-01 19:35:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,17 @@
 # code here!
-end
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
